986a0aae5fda284967ad79b6405f543f8b3998c5
Francois <fr.eiffel@gmail.com>
2018-03-26 13:20:04 -0400

    Updated MDN estimator to use the tensorflow distributions

--- cosmoml/estimator.py
+++ cosmoml/estimator.py
@@ -4,13 +4,14 @@ from __future__ import print_function
 
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
+import tensorflow.contrib.distributions as tfd
 
 from .networks import deep_mlp
 from .stats import log_gm2
 
 __all__ = ['MDNEstimator']
 
-def _mdn_model_fn(features, labels, hidden_units, n_mixture,
+def _mdn_model_fn(features, labels, hidden_units, n_mixture, diag,
                feature_columns, label_columns, optimizer, activation_fn, normalizer_fn, dropout, mode):
 
     # Check for training mode
@@ -24,34 +25,38 @@ def _mdn_model_fn(features, labels, hidden_units, n_mixture,
     net = deep_mlp(input_layer, hidden_units, activation_fn, normalizer_fn,
                    dropout, is_training)
 
-    # Create mixture components
+    # Size of the covariance matrix
+    if diag ==True:
+        size_sigma = label_dimension
+    else:
+        size_sigma = (label_dimension *(label_dimension +1) // 2)
+
+    # Create mixture components from network output
     out_mu = slim.fully_connected(net, label_dimension*n_mixture , activation_fn=None)
-    out_mu = tf.reshape(out_mu, (-1, label_dimension, n_mixture))
+    out_mu = tf.reshape(out_mu, (-1, n_mixture, label_dimension))
 
-    out_logvar = slim.fully_connected(net, label_dimension*n_mixture , activation_fn=None)
-    out_logvar = tf.clip_by_value(out_logvar, -10, 10) # For stability
-    out_logvar = tf.reshape(out_logvar, (-1, label_dimension, n_mixture))
+    out_sigma = slim.fully_connected(net, size_sigma *n_mixture, activation_fn=None)
+    out_sigma = tf.reshape(out_sigma, (-1,n_mixture, size_sigma))
 
-    out_p = slim.fully_connected(net, label_dimension*n_mixture , activation_fn=None)
-    out_p = tf.nn.softmax(tf.reshape(out_p, (-1, label_dimension, n_mixture)))
+    out_p = slim.fully_connected(net, n_mixture, activation_fn=None)
+    out_p = tf.nn.softmax(tf.reshape(out_p, (-1, n_mixture)))
 
-    predictions = {'mu': out_mu, 'sigma': tf.exp(0.5*out_logvar), 'p':out_p}
+    if diag == True:
+        sigma_mat = tf.nn.softplus(out_sigma)
+        gmm = tfd.MixtureSameFamily(mixture_distribution=tfd.Categorical(probs=out_p),
+                      components_distribution=tfd.MultivariateNormalDiag(loc=out_mu,
+                                                                        scale_diag=sigma_mat))
+    else:
+        sigma_mat = tfd.matrix_diag_transform(tfd.fill_triangular(out_sigma), transform=tf.nn.softplus)
+        gmm = tfd.MixtureSameFamily(mixture_distribution=tfd.Categorical(probs=out_p),
+                     components_distribution=tfd.MultivariateNormalTriL(loc=out_mu,
+                                                                        scale_tril=sigma_mat))
+
+    predictions = {'mu': out_mu, 'sigma': sigma_mat, 'p':out_p}
 
     if mode == tf.estimator.ModeKeys.PREDICT:
-
         # Add functionality to sample directly from the model
-        random_indices = tf.multinomial(tf.log(tf.reshape(out_p, (-1, n_mixture))), 1)
-        random_indices = tf.reshape(random_indices, (-1,))
-
-        # Create mask to select these indices
-        mask = tf.one_hot(random_indices, n_mixture, on_value = True, off_value = False, dtype = tf.bool)
-        # Select the Gaussians used in the sampling
-        mu = tf.boolean_mask(tf.reshape(out_mu, (-1, n_mixture)), mask)
-        logvar =  tf.boolean_mask(tf.reshape(out_logvar, (-1, n_mixture)), mask)
-
-        # Sample from Gaussian
-        y = mu + tf.exp(0.5 * logvar) * tf.random_normal(tf.shape(mu))
-        y = tf.reshape(y, (-1, label_dimension))
+        y = gmm.sample()
 
         samples = {}
         for i,k in enumerate(label_columns):
@@ -65,9 +70,8 @@ def _mdn_model_fn(features, labels, hidden_units, n_mixture,
     label_layer = tf.feature_column.input_layer(features=features, feature_columns=label_columns)
 
     # Compute and register loss function
-    loss = - tf.reduce_mean(log_gm2(label_layer, out_mu, out_logvar, out_p))
+    loss = - tf.reduce_mean(gmm.log_prob(label_layer),axis=0)
     tf.losses.add_loss(loss)
-
     total_loss = tf.losses.get_total_loss()
 
     train_op = None
@@ -98,6 +102,7 @@ class MDNEstimator(tf.estimator.Estimator):
                label_columns,
                hidden_units,
                n_mixture,
+               diagonal=True,
                optimizer=tf.train.AdamOptimizer,
                activation_fn=tf.nn.relu,
                normalizer_fn=slim.batch_norm,
@@ -108,7 +113,7 @@ class MDNEstimator(tf.estimator.Estimator):
         """
 
         def _model_fn(features, labels, mode):
-            return _mdn_model_fn(features, labels, hidden_units, n_mixture,
+            return _mdn_model_fn(features, labels, hidden_units, n_mixture, diagonal,
                feature_columns, label_columns, optimizer, activation_fn, normalizer_fn, dropout, mode)
 
         super(self.__class__, self).__init__(model_fn=_model_fn,
